73a39bdb26175d5d955aa0ab873ecef9800626c6
yahyacodes <yahya.codin@gmail.com>
2023-09-29 16:33:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => {});
+
+const form = document.querySelector("#create-task-form");
+const input = document.querySelector("#new-task-description");
+const taskBtn = document.querySelector(".taskBtn");
+
+// Prevent form default
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Add new taks to list and clear the input field
+taskBtn.addEventListener("click", () => {
+  // Add new task to list
+  const tasks = document.querySelector("#tasks");
+  const h2 = document.createElement("h2");
+  h2.innerHTML = input.value;
+  tasks.appendChild(h2);
+
+  // Clear input field
+  input.value = "";
 });
